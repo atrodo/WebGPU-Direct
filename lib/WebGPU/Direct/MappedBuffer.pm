a78c3c6e5a2bf1d879c5d412075b2c1b8e64e6b6
Jon Gentle <atrodo@atrodo.org>
2024-04-17 23:30:32 -0400

    Add bare-bones docs for MappedBuffer type

---
+++ lib/WebGPU/Direct/MappedBuffer.pm
@@ -0,0 +1,43 @@
+package WebGPU::Direct::MappedBuffer
+{
+  use v5.30;
+  use warnings;
+  no warnings qw(experimental::signatures);
+  use feature 'signatures';
+};
+
+1;
+__END__
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+WebGPU::Direct::MappedBuffer
+
+=head2 Methods
+
+=head3 buffer
+
+=over
+
+=item * Return Type
+
+=over
+
+=item * String (void *)
+
+=back
+
+=item * Arguments
+
+=over
+
+=item * value (String (void *))
+
+=back
+
+=back
+
+
